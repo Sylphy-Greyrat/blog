966cef3443e82b0f1d90787188a640f58da134dc
1752773927 <me20030615@gmail.com>
2023-12-24 23:26:54 +0800

    初始化3

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,6 +1,7 @@
 import "vuepress-theme-hope/presets/bounce-icon.scss"
 import "vuepress-theme-hope/presets/shinning-feature-panel.scss"
-import { defineClientConfig } from "@vuepress/client";
+import {defineClientConfig} from "@vuepress/client";
+
 export default defineClientConfig({
     // enhance: ({ app, router, siteData }) => {
     //     app.component("MyComponent", MyComponent);
